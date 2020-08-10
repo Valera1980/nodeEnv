2bb5c04b2329b9f3761a10a73e365bbe9d81cf4b
Valera <gavrilow777@gmail.com>
2020-08-10 20:16:02 +0300

    add dotenv-flow

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-require('dotenv').config();
+// require('dotenv').config();
+const hhh = require('dotenv-flow').config();
+
 import { funcOne } from "./function-one";
-const arr: number[] = [1,2,4,5];
+const arr: number[] = [1, 2, 4, 5];
 
 const a = (param: number[]) => {
-   for(const item of param){
-       console.log(item);
+   for (const item of param) {
+      console.log(item);
    }
-//    console.log(process.env);
-   const flag = process.argv.find(argument => argument == 'ENV');
-   console.log(flag);
+
+   console.log('database SOME_VAL_ONE:', process.env.SOME_VAL_ONE);
+   console.log('database SOME_VAL_TWO:', process.env.SOME_VAL_TWO);
+   console.log('database SOME_VAL_THREE:', process.env.SOME_VAL_THREE);
+   console.log('database SOME_VAL_FOUR:', process.env.SOME_VAL_FOUR);
+   console.log('database callTrackingDbConnection:', process.env.callTrackingDbConnection);
+   console.log('database hhh:', hhh);
 }
 
 a(arr);
