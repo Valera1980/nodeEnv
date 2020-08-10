29195c1dccb3706480fc0758aa2fa71a225e7bf7
Valera <gavrilow777@gmail.com>
2020-08-10 17:59:40 +0300

    add env

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ const a = (param: number[]) => {
    for(const item of param){
        console.log(item);
    }
-   console.log(process.env);
+//    console.log(process.env);
+   const flag = process.argv.find(argument => argument == 'ENV');
+   console.log(flag);
 }
 
 a(arr);
